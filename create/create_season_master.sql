9b2cbedb6ad9907970acf9d2308cc181bce4fb06
ryuseim1115 <ryusei.maebuchi@gmail.com>
2026-02-06 03:15:52 +0900

    insert文に関して

--- create/create_season_master.sql
+++ create/create_season_master.sql
@@ -4,5 +4,5 @@ CREATE TABLE
         show_id INT not null comment '番組ID',
         season_name VARCHAR(100) not null comment 'シーズン名',
         PRIMARY key (season_id),
-        constraint fk_season_master foreign key (season_id) references show_master (show_id) on delete cascade
+        constraint fk_season_master foreign key (show_id) references show_master (show_id) on delete cascade
     ) COMMENT = 'シーズンマスタ';
--- main.ps1
+++ main.ps1
@@ -27,10 +27,38 @@ Write-Host 'table:season_master setup completed'
 Get-Content create/create_episode_master.sql -Encoding utf8 | mysql --login-path=term2 --default-character-set=utf8mb4 $DbName
 Write-Host 'table:episode_master setup completed'
 
+Get-Content create/create_time_ch.sql -Encoding utf8 | mysql --login-path=term2 --default-character-set=utf8mb4 $DbName
+Write-Host 'table:time_ch setup completed'
+
 Get-Content create/create_view_info.sql -Encoding utf8 | mysql --login-path=term2 --default-character-set=utf8mb4 $DbName
 Write-Host 'table:view_info setup completed'
 
 Get-Content insert/insert_show_master.sql -Encoding utf8 | mysql --login-path=term2 --default-character-set=utf8mb4 $DbName
-Write-Host 'table:view_info setup completed'
+Write-Host 'insert:show_master completed'
+
+Get-Content insert/insert_genre_master.sql -Encoding utf8 | mysql --login-path=term2 --default-character-set=utf8mb4 $DbName
+Write-Host 'insert:genre_master completed'
+
+Get-Content insert/insert_show_genre.sql -Encoding utf8 | mysql --login-path=term2 --default-character-set=utf8mb4 $DbName
+Write-Host 'insert:show_genre completed'
+
+Get-Content insert/insert_season_master.sql -Encoding utf8 | mysql --login-path=term2 --default-character-set=utf8mb4 $DbName
+Write-Host 'insert:season_master completed'
+
+Get-Content insert/insert_episode_master.sql -Encoding utf8 | mysql --login-path=term2 --default-character-set=utf8mb4 $DbName
+Write-Host 'insert:episode_master completed'
+
+Get-Content insert/insert_time_master.sql -Encoding utf8 | mysql --login-path=term2 --default-character-set=utf8mb4 $DbName
+Write-Host 'insert:time_master completed'
+
+Get-Content insert/insert_ch_master.sql -Encoding utf8 | mysql --login-path=term2 --default-character-set=utf8mb4 $DbName
+Write-Host 'insert:ch_master completed'
+
+Get-Content insert/insert_time_ch.sql -Encoding utf8 | mysql --login-path=term2 --default-character-set=utf8mb4 $DbName
+Write-Host 'insert:time_ch completed'
+
+Get-Content insert/insert_view_info.sql -Encoding utf8 | mysql --login-path=term2 --default-character-set=utf8mb4 $DbName
+Write-Host 'insert:view_info completed'
+
 
 Write-Host 'All setup completed'
