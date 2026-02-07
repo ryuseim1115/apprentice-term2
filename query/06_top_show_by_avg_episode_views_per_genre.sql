bf5e28a20450c35959dfff4d70fe33f287852c18
ryuseim1115 <ryusei.maebuchi@gmail.com>
2026-02-08 01:00:10 +0900

    クエリ６

--- query/06_top_show_by_avg_episode_views_per_genre.sql
+++ query/06_top_show_by_avg_episode_views_per_genre.sql
@@ -1,11 +1,9 @@
--- ウィンドウ関数なし　（ジャンル、番号ごと平均視聴数の最大が2つ以上あれば、重複分だけレコード出力される）
 select
-    ge.genre_id,
-    show_title_avg.show_title,
-    show_title_avg.avg_view_cnt
+    show_title_avg.genre_name as 'ジャンル名',
+    show_title_avg.show_title as '番組タイトル',
+    show_title_avg.avg_view_cnt as 'エピソード平均視聴数'
 from
-    genre_master as ge
-    left join (
+    (
         select
             show_title.genre_id,
             max(show_title.avg_view_cnt) as max_view_cnt
@@ -26,11 +24,12 @@ from
             ) show_title
         group by
             show_title.genre_id
-    ) show_title_max on show_title_max.genre_id = ge.genre_id
+    ) show_title_max
     left join (
         select
             sh.show_title,
             shg.genre_id,
+            gm.genre_name,
             avg(vi.view_cnt) as avg_view_cnt
         from
             show_master as sh
@@ -38,9 +37,13 @@ from
             left join episode_master as ep on ep.season_id = se.season_id
             left join view_info as vi on vi.episode_id = ep.episode_id
             left join show_genre as shg on shg.show_id = sh.show_id
+            left join genre_master as gm on gm.genre_id = shg.genre_id
         group by
             sh.show_title,
             sh.show_id,
-            shg.genre_id
+            shg.genre_id,
+            gm.genre_name
     ) show_title_avg on show_title_avg.genre_id = show_title_max.genre_id
     and show_title_avg.avg_view_cnt = show_title_max.max_view_cnt
+order by
+    show_title_avg.genre_id;
